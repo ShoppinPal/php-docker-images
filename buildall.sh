f99846a1572ba162d045d6ec6a47228128100d80
Zsolt Szeberenyi <zsolt@szeberenyi.com>
2016-09-06 17:32:39 +1200

    Initial import

---
+++ buildall.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+docker build -t shoppinpal/php-base shoppinpal/php-base
+docker build -t shoppinpal/php-base-fpm shoppinpal/php-base-fpm
+docker build -t shoppinpal/php-soap-base shoppinpal/php-soap-base
+docker build -t shoppinpal/php-soap-base-fpm shoppinpal/php-soap-base-fpm
