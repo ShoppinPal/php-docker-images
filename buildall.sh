60bf0a29a3154e11e7afddda66da90aaf9537b80
Zsolt Szeberenyi <zsolt@szeberenyi.com>
2017-10-10 21:35:20 +1300

    Add nginx-php-ci image

--- buildall.sh
+++ buildall.sh
@@ -3,3 +3,4 @@ docker build -t shoppinpal/php-base shoppinpal/php-base
 docker build -t shoppinpal/php-base-fpm shoppinpal/php-base-fpm
 docker build -t shoppinpal/php-soap-base shoppinpal/php-soap-base
 docker build -t shoppinpal/php-soap-base-fpm shoppinpal/php-soap-base-fpm
+docker build -t shoppinpal/nginx-php-ci shoppinpal/nginx-php-ci
