40537a45de89cbac68b8dda19aa91bf60fb7ff86
Zsolt Szeberenyi <zsolt@szeberenyi.com>
2018-07-07 16:43:58 +1200

    Fix robo, add auto push

--- buildall.sh
+++ buildall.sh
@@ -4,3 +4,9 @@ docker build -t shoppinpal/php-base-fpm shoppinpal/php-base-fpm
 docker build -t shoppinpal/php-soap-base shoppinpal/php-soap-base
 docker build -t shoppinpal/php-soap-base-fpm shoppinpal/php-soap-base-fpm
 docker build -t shoppinpal/nginx-php-ci shoppinpal/nginx-php-ci
+
+docker push shoppinpal/php-base
+docker push shoppinpal/php-base-fpm
+docker push shoppinpal/php-soap-base
+docker push shoppinpal/php-soap-base-fpm
+docker push shoppinpal/nginx-php-ci
