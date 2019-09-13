bec34e1288c25f4abc11e0eda232778d5e19a984
Zsolt Szeberenyi <zsolt@szeberenyi.com>
2019-09-13 19:12:34 +1200

    Add php ci images

--- buildall.sh
+++ buildall.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
+
+set -e
+
 docker pull ubuntu:xenial
 docker pull php:7.1-fpm
 docker pull php:7.2-fpm
 docker pull php:7.3-fpm
+docker pull swaggerapi/swagger-codegen-cli-v3:3.0.11
 
 docker build -t shoppinpal/git-ssh shoppinpal/git-ssh
 
@@ -20,6 +24,8 @@ docker build -t shoppinpal/php-ci:7.3 shoppinpal/php-ci/7.3
 
 docker build -t shoppinpal/nginx-php-ci shoppinpal/nginx-php-ci
 
+docker build -t shoppinpal/swagger-codegen shoppinpal/swagger-codegen
+
 # Deprecated images
 docker build -t shoppinpal/php-base shoppinpal/php-base
 docker build -t shoppinpal/php-base-fpm shoppinpal/php-base-fpm
@@ -32,6 +38,8 @@ docker push shoppinpal/php-ci
 
 docker push shoppinpal/git-ssh
 docker push shoppinpal/nginx-php-ci
+
+docker push shoppinpal/swagger-codegen
 
 # Deprecated images
 docker push shoppinpal/php-base
