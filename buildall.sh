a2c3e782fa340e68380b7cd32cebb48bf89e4890
Zsolt Szeberenyi <zsolt@szeberenyi.com>
2020-02-22 17:18:52 +1300

    Add php 7.4 images

--- buildall.sh
+++ buildall.sh
@@ -6,6 +6,7 @@ docker pull ubuntu:xenial
 docker pull php:7.1-fpm
 docker pull php:7.2-fpm
 docker pull php:7.3-fpm
+docker pull php:7.4-fpm
 docker pull swaggerapi/swagger-codegen-cli-v3:3.0.11
 
 docker build -t shoppinpal/git-ssh shoppinpal/git-ssh
@@ -21,6 +22,10 @@ docker build -t shoppinpal/php-ci:7.2 shoppinpal/php-ci/7.2
 docker build -t shoppinpal/php-fpm:7.3 shoppinpal/php-fpm/7.3
 docker build -t shoppinpal/php-fpm-dev:7.3 shoppinpal/php-fpm-dev/7.3
 docker build -t shoppinpal/php-ci:7.3 shoppinpal/php-ci/7.3
+
+docker build -t shoppinpal/php-fpm:7.4 shoppinpal/php-fpm/7.4
+docker build -t shoppinpal/php-fpm-dev:7.4 shoppinpal/php-fpm-dev/7.4
+docker build -t shoppinpal/php-ci:7.4 shoppinpal/php-ci/7.4
 
 docker build -t shoppinpal/nginx-php-ci shoppinpal/nginx-php-ci
 
