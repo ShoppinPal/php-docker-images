0cf2c2dfbc8c06965f54ce9427f923bfad1473e4
Zsolt Szeberenyi <zsolt@szeberenyi.com>
2021-08-15 17:45:11 +1200

    Add php 8.0-alpine images

--- buildall.sh
+++ buildall.sh
@@ -9,6 +9,7 @@ docker pull php:7.2-fpm
 docker pull php:7.3-fpm
 docker pull php:7.4-fpm
 docker pull php:8.0-fpm
+docker pull php:8.0-fpm-alpine
 docker pull swaggerapi/swagger-codegen-cli-v3:3.0.20
 docker pull bitnami/fluentd:1.12.3-debian-10-r4
 
@@ -32,6 +33,10 @@ docker build -t shoppinpal/php-fpm-azure:8.0 shoppinpal/php-fpm-azure/8.0
 docker build -t shoppinpal/php-fpm-dev:8.0 shoppinpal/php-fpm-dev/8.0
 docker build -t shoppinpal/php-ci:8.0 shoppinpal/php-ci/8.0
 
+docker build -t shoppinpal/php-fpm:8.0-alpine shoppinpal/php-fpm/8.0-alpine
+docker build -t shoppinpal/php-fpm-dev:8.0-alpine shoppinpal/php-fpm-dev/8.0-alpine
+docker build -t shoppinpal/php-ci:8.0-alpine shoppinpal/php-ci/8.0-alpine
+
 docker build -t shoppinpal/nginx-php-ci shoppinpal/nginx-php-ci
 
 docker build -t shoppinpal/swagger-codegen shoppinpal/swagger-codegen
@@ -44,15 +49,18 @@ docker push shoppinpal/mysql-tmpfs
 docker push shoppinpal/php-fpm:7.3
 docker push shoppinpal/php-fpm:7.4
 docker push shoppinpal/php-fpm:8.0
+docker push shoppinpal/php-fpm:8.0-alpine
 docker push shoppinpal/php-fpm-azure:7.3
 docker push shoppinpal/php-fpm-azure:7.4
 docker push shoppinpal/php-fpm-azure:8.0
 docker push shoppinpal/php-fpm-dev:7.3
 docker push shoppinpal/php-fpm-dev:7.4
 docker push shoppinpal/php-fpm-dev:8.0
+docker push shoppinpal/php-fpm-dev:8.0-alpine
 docker push shoppinpal/php-ci:7.3
 docker push shoppinpal/php-ci:7.4
 docker push shoppinpal/php-ci:8.0
+docker push shoppinpal/php-ci:8.0-alpine
 
 docker push shoppinpal/git-ssh
 docker push shoppinpal/nginx-php-ci
